68418ee295ab7f8e42572a4f6de6e84da8606582
Johnny Bowman <JohnnyBowman@gmail.com>
2013-04-18 14:25:52 -0400

    Can make a phone call to someone. Person called receives a message from a robo-lady reading a script

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -55,21 +55,55 @@ class SuggestionsController < ApplicationController
     redirect_to suggestions_path
   end
 
-   def call
-       # put your own credentials here - from twilio.com/user/account
-    
-    account_sid = 'AC582454da60c53d2660fbff36042656e6'
-    auth_token = 'fa4e593e6e396ecbac9d3fd33eba7b2f'
+
+# twilio code that calls person and reads script from XML file
+  # def call
+  #      # put your own credentials here - from twilio.com/user/account  
+  #   account_sid = 'AC3ecb799e792404580fe5e903b88d3929'
+  #   auth_token = 'd5ee548232ded22642dfe296d46df3af'
  
-    # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new account_sid, auth_token
+  #   # set up a client to talk to the Twilio REST API
+  #   @client = Twilio::REST::Client.new account_sid, auth_token
  
-    @call = @client.account.calls.create(
-    :from => '+13109075542',   # From your Twilio number
-    :to => '+13108041305',     # To any number
-    # Fetch instructions from this URL when the call connects
-    :url => 'https://demo.twilio.com/welcome/voice/'
-    )
-    redirect '/'
-  end
+  #   @call = @client.account.calls.create(
+  #   :from => '+13109075542',   # From your Twilio number
+  #   :to => '+13108041305',     # To any number
+  #   # Fetch instructions from this URL when the call connects
+  #   :url => 'https://demo.twilio.com/welcome/voice/'
+  #   )
+  #   redirect_to '/'
+  # end
+
+twilio code that calls person and reads script from XML file
+  def call
+    # Set our AccountSid and AuthToken
+    AccountSid = "AC3ecb799e792404580fe5e903b88d3929"
+    AuthToken = "d5ee548232ded22642dfe296d46df3af"
+ 
+    :from => '+13108041305',   #Your Twilio Number or an Outgoing Caller ID you have previously validated
+    with Twilio
+    :to => '+16462837437',     #Number you wish to call
+ 
+    # Directory location for callback.php file (for use in REST URL)
+    $url = 'http://www.example.com/clicktocall/'
+ 
+    # Instantiate a new Twilio Rest Client
+    $client = new Services_Twilio($AccountSid, $AuthToken)
+ 
+    if (!isset($_REQUEST['called'])) {
+      $err = urlencode("Must specify your phone number")
+      header("Location: index.php?msg=$err")
+      die;
+    }
+ 
+    # make Twilio REST request to initiate outgoing call
+    $call = $client->account->calls->create($from, $to, $url . 'callback.php?number=' . $_REQUEST['called']);
+ 
+    # redirect back to the main page with CallSid
+    $msg = urlencode("Connecting... ".$call->sid);
+    header("Location: index.php?msg=$msg");
+
+    redirect_to '/'
+    end
+  
 end
